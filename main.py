65cf04c2ec7e24b327d2c4531f60d991874fd7c4
Jekyll-007 <teenofeejay@gmail.com>
2024-10-29 18:00:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#empty tuple
+my_tuple=()
+print(my_tuple)
+#number tuple
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+#mixes tuple
+my_tuple=('apple','banana','cherry',1,2,3)
+print(my_tuple)
+print(my_tuple[1])
+#slicing
+print(my_tuple[2:4])
